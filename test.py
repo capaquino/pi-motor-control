b39d231668cfc0babe95f301f46e4da29997928d
Nathan <nad46@zips.uakron.edu>
2019-01-21 01:18:35 -0800

    Added stopwatch feature and comments about microstepping.

--- test.py
+++ test.py
@@ -1,24 +1,35 @@
 import RPi.GPIO as gpio
-from time import sleep
+from time import sleep, time
 
 
 step_pin = 16
 dir_pin = 18
-SLEEP_TIME = 0.0001
+SLEEP_TIME = 0.001 #0.0001 for microstepping, how is this determined to not torque out.
+direction = False # clockwise?
+rotations = 2
 
 gpio.setmode(gpio.BOARD)
 gpio.setup(step_pin, gpio.OUT)
 gpio.setup(dir_pin, gpio.OUT)
 
-gpio.output(dir_pin, False)
-distance = 0
+gpio.output(dir_pin, direction)
+steps = 0
 
-while distance < 3200:
+start = time()
+
+#  NEMA-17 Stepper Motor
+#  200  full stepping
+#  3200 1/16 microstepping
+
+while steps < 3200*rotations:
 	gpio.output(step_pin, True)
 	sleep(SLEEP_TIME)
 	gpio.output(step_pin, False)
 	sleep(SLEEP_TIME)
-	distance = distance + 1
+	steps = steps + 1
+
+end = time()
+print("{0} rotations took {1}s".format(rotations, end-start))
 
 gpio.cleanup()
 
