f5eda6aebb4278547541116397e0f8cc69b1893d
Nathan <nad46@zips.uakron.edu>
2019-02-08 21:16:04 -0800

    Added code and comments to optimize use with coupler to curtains

--- test.py
+++ test.py
@@ -1,35 +1,33 @@
 import RPi.GPIO as gpio
 from time import sleep, time
 
-
 step_pin = 16
 dir_pin = 18
-SLEEP_TIME = 0.001 #0.0001 for microstepping, how is this determined to not torque out.
-direction = False # clockwise?
-rotations = 2
 
+SLEEP_TIME = 0.0001
+direction = False # T:clockwise&close, F:counter-clockwise&open
 gpio.setmode(gpio.BOARD)
 gpio.setup(step_pin, gpio.OUT)
 gpio.setup(dir_pin, gpio.OUT)
 
-gpio.output(dir_pin, direction)
-steps = 0
+for x in range(6):
+	gpio.output(dir_pin, direction)
+	steps = 0
+	# start = time()
+	#  NEMA-17 Stepper Motor
+	#  200  full stepping
+	#  3200 1/16 microstepping
 
-start = time()
+	while steps < 3200*3:
+		gpio.output(step_pin, True)
+		sleep(SLEEP_TIME)
+		gpio.output(step_pin, False)
+		sleep(SLEEP_TIME)
+		steps = steps + 1
+	direction = not direction
 
-#  NEMA-17 Stepper Motor
-#  200  full stepping
-#  3200 1/16 microstepping
-
-while steps < 3200*rotations:
-	gpio.output(step_pin, True)
-	sleep(SLEEP_TIME)
-	gpio.output(step_pin, False)
-	sleep(SLEEP_TIME)
-	steps = steps + 1
-
-end = time()
-print("{0} rotations took {1}s".format(rotations, end-start))
+	# end = time()
+	# print("{0} steps took {1}s".format(steps, end-start))
 
 gpio.cleanup()
 
