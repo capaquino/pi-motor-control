cae8fc28eaee20f5ee9d5dc014e9b699600670b0
Nathan_pi <nad46@zips.uakron.edu>
2019-02-10 03:51:56 -0800

    Added board-to-BCM dictionary and enum. Added stub for MoveAbs function.

--- stepper.py
+++ stepper.py
@@ -1,7 +1,72 @@
+# Applies python3 style division to module
+from __future__ import division
+
 import gpiozero
 from time import sleep
-# from enum import Enum # for init types?
+from enum import Enum
 
+BoardPins = {
+	3 : 2, # SDA
+	5 : 3, # SCL
+	7 : 4, # GPCLK0
+	8 : 14, # TXD
+	10 : 15, # RXD
+	11 : 17,
+	12 : 18, # PWM0
+	13 : 27,
+	15 : 22,
+	16 : 23,
+	18 : 24,
+	19 : 10, # MOSI
+	21 : 9, # MISO
+	22 : 25,
+	23 : 11, # SCLK
+	24 : 8, # CE0
+	26 : 7, # CE1
+	27 : 0, # EEPROM Data
+	28 : 1, # EEPROM Clock
+	29 : 5,
+	31 : 6,
+	32 : 12, # PWM0
+	33 : 13, # PWM1
+	35 : 19, # MISO
+	36 : 16,
+	37 : 26,
+	38 : 20, # MOSI
+	40 : 21 # SCLK
+}
+
+# Call using Board.PXX.value
+# Don't really like the syntax as much as the dictionary.
+class Board(Enum):
+	P3 = 2 # SDA
+	P5 = 3 # SCL
+	P7 = 4 # GPCLK0
+	P8 = 14 # TXD
+	P10 = 15 # RXD
+	P11 = 17
+	P12 = 18 # PWM0
+	P13 = 27
+	P15 = 22
+	P16 = 23
+	P18 = 24
+	P19 = 10 # MOSI
+	P21 = 9 # MISO
+	P22 = 25
+	P23 = 11 # SCLK
+	P24 = 8 # CE0
+	P26 = 7 # CE1
+	P27 = 0 # EEPROM Data
+	P28 = 1 # EEPROM Clock
+	P29 = 5
+	P31 = 6
+	P32 = 12 # PWM0
+	P33 = 13 # PWM1
+	P35 = 19 # MISO
+	P36 = 16
+	P37 = 26
+	P38 = 20 # MOSI
+	P40 = 21 # SCLK
 
 class Stepper(object):
 	def __init__(self, stepPin_pin, dirPin_pin, frequency_hz):
@@ -41,10 +106,11 @@ class Stepper(object):
 				dir = gpiozero.DigitalOutputDevice(self.dirPin) # initial_value = False
 			# Do move
 			while distMoved_steps < abs(moveDist_steps):
+				# Generate pulse train
 				pulse.on()
-				sleep(1/(self.frequency*2))
+				sleep( 1 / (self.frequency * 2) )
 				pulse.off()
-				sleep(1/(self.frequency*2))
+				sleep( 1.0 / (self.frequency * 2) )
 				# Update relevant values
 				distMoved_steps += 1
 
@@ -52,7 +118,7 @@ class Stepper(object):
 			# Re-raise exception
 			raise
 		finally:
-			# Update current position
+			# Update current position with actual distance moved
 			if dir.value > 0:
 				self.currentPosition += distMoved_steps
 			else:
@@ -63,13 +129,14 @@ class Stepper(object):
 			pulse.close()
 			dir.close()
 
-# TODO Make a BOARD pin numbering library or enum for GPIO. gpiozero uses BCM numbering
+	def MoveAbs(self, targetPosition_steps):
+		pass
 
-
+# gpiozero uses BCM numbering, not changeable to BOARD numbering
 # BCM 23 = step pin = 16
 # BCM 24 = dir pin = 18
 if __name__ == '__main__':
-	s = Stepper(23, 24, 1000)
+	s = Stepper(Board[16], Board[18], 3200)
 	s.MoveRel(3200)
 	print(s.currentPosition)
 	s.MoveRel(3200)
